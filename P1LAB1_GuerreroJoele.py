945d7d6adb2696e4693c14512dbd73e511b7eb73
guerrerojn <jnguerrero@protonmail.com>
2024-11-26 11:41:55 -0500

    Add files via upload

---
+++ P1LAB1_GuerreroJoele.py
@@ -0,0 +1,20 @@
+ # Joele Guerrero
+ # 11/15/2024
+ # P1LAB1
+ # Welcme to CTI-110
+
+
+'''
+Joele Guerrero
+11/15/2024
+P1LAB1
+Welcme to CTI-110
+'''
+
+
+first = input (" Enter your first name: ")
+print()
+last = input (" Enter your last name: ")
+print()
+welcome = input (" Hello, Welcome to CTI-110! ")
+print()
