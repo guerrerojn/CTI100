3e3f0458394b0dd88bb5a597b6a15291ce79f0e0
guerrerojn <jnguerrero@protonmail.com>
2024-12-05 11:32:45 -0500

    Add files via upload

---
+++ P2HW1_JoeleGuerrero.py
@@ -0,0 +1,42 @@
+  # Joele Guerrero
+  # 12/05/2024
+  # P2HW1
+  # This program calculates and displays travel expenses
+
+
+'''
+Joele Guerrero
+12/05/2024
+P1HW2
+This program calculates and displays travel expenses
+'''
+
+
+print (" This program calculates and displays travel expenses.\n")
+budget = float (input(" Enter budget: "))
+print()
+destination = input(" Enter your destination: ")
+print()
+gas = float(input(" How much do you think you will spend on gas? "))
+print()
+hotel = float(input(" How much will you need for a hotel? "))
+print()
+food = float( input(" Last, how much do you think you will need for food? "))
+print()
+
+
+travel = print("------------Travel Expenses------------")
+print()
+location = input(" location: ")
+print()
+total = float(input(" Initial Budget: "))
+print()
+fuel = float(input(" Fuel: "))
+print()
+accomodation = float(input(" Hotel: "))
+print()
+food = float(input(" Food: "))
+print()
+print = (" ---------------------------------------")
+
+balance = float(input(" Remaining Balance: "))
